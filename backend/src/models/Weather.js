199af9705d2bcaaecf7bc91392f945d1851d48f7
douglasvcv <douglasvitor1233@gmail.com>
2024-12-22 23:07:55 -0300

    building model

--- backend/src/models/Weather.js
+++ backend/src/models/Weather.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
+import { connectDB } from '../config/db'
 
-async function createWeatherData(){
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const weatherSchema = mongoose.Schema({
+    city:{type:String, required:true},
+    temperature:{type:Number, required:true},
+    humidity:{type:Number, required:true},
+    description:{type:String, required:true},
+    timestamp:{type:Date, default:Date.now},
+})
+
+const WeatherCreate = mongoose.model('Weather', weatherSchema)
+
+module.exports = WeatherCreate
