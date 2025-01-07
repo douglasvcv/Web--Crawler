5ee1494038a2d0b0b1f32d9b0cdd1669a38ab135
douglasvcv <douglasvitor1233@gmail.com>
2025-01-06 22:43:18 -0300

    building weatherRoutes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
 //Base of everything
-import {app} from "./server.js"
-
+import {Rotas} from "./routes/weatherRoutes.js"
+import express from 'express'
 import {GetWeatherData} from "./controller/weatherController.js"
 import {weatherService} from './services/weatherService.js'
+import { app } from "./server.js"
 
-app.get('/', async (req, res)=>{
-    GetWeatherData(req, res)
-})
+app.use(express.json())
+app.use(Rotas)
 
 app.listen(8080, ()=>{
     console.log("Server running http://localhost:8080")
--- backend/src/routes/weatherRoutes.js
+++ backend/src/routes/weatherRoutes.js
@@ -1,9 +1,24 @@
-//Continue Here
+//Routes of my server
 
+import {Router} from 'express'
 
-import { app } from '../server.js'
+export const Rotas = Router()
 
-export const router = app.route()
+Rotas.get('/', (req, res)=>{
+    res.send("Home page!")
+})
 
+//Rota para mostrar a requisição de alguma cidade
+Rotas.get('/previsao', (req, res)=>{
+    res.send('Get weather data')
+})
 
+//Rota para criar a previsão no banco de dados caso os dados tenha sidos requiridos da api
+Rotas.post('/create', (req, res)=>{
+    
+})
 
+//Rota para excluir previsão do passado 
+Rotas.delete('/delete', (req, res)=>{
+
+})
